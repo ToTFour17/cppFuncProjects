134b561c883f4d75f4c37b3fce309cb01a5aea8b
Timur <145016191+ToTFour17@users.noreply.github.com>
2023-09-28 14:30:20 +0300

    create calc(switch case)

---
+++ calc(switch case).cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	float num1, num2;
+    char operation;
+	cin >> num1>>num2;
+	cin >> operation;
+	switch (operation) {
+	case '+':
+		cout << num1 + num2;
+		break;
+	case '-':
+		cout << num1 - num2;
+		break;
+	case '*':
+		cout << num1 * num2;
+		break;
+	case '/':
+		cout << num1 / num2;
+		break;
+	default:
+		cout << "Nevernoe";
+
+		if (num1 == 0)
+			cout << "Oshibka";
+		else
+			cout << "Oshibka";
+
+
+
+
+
+
+
+
+
+	}
+
+
+}
